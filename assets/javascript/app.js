0aa4854cf2b60c171f0637654106a74afbbd59e6
ThinMintz <rachel.rodriguez05@gmail.com>
2017-10-06 22:08:32 -0500

    tryiing to get the start button to hide and contents to show when clicked

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+/*game starts with a start button, that starts the game 
+(and timer) when clicked*/
+var startGame()
+
+$("Button").click(function() {
+	$("h3").hide();
+});
+
+
+
+
+/*setTimeout(timeUp, 1000 * 15);
+
+function timeUp() {
+
+  console.log("done");
+  $("#time-left").append("<h2>Time's Up!</h2>");
+  console.log("time is up");
+}
+*/
